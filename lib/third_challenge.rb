f048f22f87f5cb75ce0f04a6dbcb27c6f564d84d
Dina Hafez <dinahafez.8@gmail.com>
2018-07-07 20:30:36 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -19,7 +19,9 @@ def third_challenge
         },
       :hero_friends => [
         {
-          
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
         },
         {
           
